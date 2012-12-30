2f5c8294234e9be90b275fe1038c6f7cabefaf8f
jauhaf <joohap@gmail.com>
2012-12-30 12:27:51 +0700

    update default data access report

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -91,43 +91,43 @@ class User_Controller extends Secure_Controller {
         if($id===null) {
             return Redirect::to('user/index');
         }
-		$validation = Validator::make(Input::all(), $this->getRules('edit'));
+        $validation = Validator::make(Input::all(), $this->getRules('edit'));
         $user = User::find($id);
         $userdata = Input::all();
 //        {dd($user->picture );}
         if(!$validation->fails()) {
-			if(User::unique_login_id($userdata['login_id'], $id)) {
+            if(User::unique_login_id($userdata['login_id'], $id)) {
                 if (Input::file('picture.name') != null && Input::file('picture.name') != '' ) {
                     //===clean prev image====
                     if ($user->picture != null && $user->picture != '') {
-                        File::delete('public/images/uploads/user/'.$user->picture);
+                        File::delete('../backend/images/uploads/user'.$user->picture);
                     }
                     //===uploading image=====
                     $filename = Str::random(32) . '.' . File::extension(Input::file('picture.name'));
-                    Input::upload('picture', 'public/images/uploads/user', $filename);
+                    Input::upload('picture', '../backend/images/uploads/user', $filename);
                     $userdata['picture'] = $filename;
                 } else {
                     $userdata['picture']='';
                 }
 
-				$success = User::update($id, $userdata);
-				if($success) {
-					//success login
-					Session::flash('message', 'Success update user');
-					return Redirect::to('user/index');
-				} else {
-					Session::flash('message_error', 'Failed to update user');
-					return Redirect::to_action('user@edit', array($id));
-				}
-			} else {
-				Session::flash('message_error', 'Login Id is already in use');
-				return Redirect::to_action('user@edit', array($id));
-			}
-		} else {
-			Log::info('Validation fails. error : ' + print_r($validation->errors, true));
+                $success = User::update($id, $userdata);
+                if($success) {
+                    //success login
+                    Session::flash('message', 'Success update user');
+                    return Redirect::to('user/index');
+                } else {
+                    Session::flash('message_error', 'Failed to update user');
+                    return Redirect::to_action('user@edit', array($id));
+                }
+            } else {
+                Session::flash('message_error', 'Login Id is already in use');
+                return Redirect::to_action('user@edit', array($id));
+            }
+        } else {
+            Log::info('Validation fails. error : ' + print_r($validation->errors, true));
             return Redirect::to_action('user@edit', array($id))
                 ->with_errors($validation);
-		}
+        }
     }
 
     public function get_add() {
@@ -145,36 +145,36 @@ class User_Controller extends Secure_Controller {
         $validation = Validator::make(Input::all(), $this->getRules());
         $userdata = Input::all();
         if(!$validation->fails()) {
-			if(User::unique_login_id($userdata['login_id'])) {
+            if(User::unique_login_id($userdata['login_id'])) {
                 if (Input::file('picture.name') != null && Input::file('picture.name') != '' ) {
                     //===uploading image=====
                     $filename = Str::random(32) . '.' . File::extension(Input::file('picture.name'));
-                    Input::upload('picture', 'public/images/uploads/user', $filename);
+                    Input::upload('picture', '../backend/images/uploads/user', $filename);
                     $userdata['picture'] = $filename;
                 } else {
                     $userdata['picture']='';
                 }
-				$success = User::create($userdata);
-				if($success) {
-					//success login
-					Session::flash('message', 'Success to create new user');
-					return Redirect::to('user/index');
-				} else {
-					Session::flash('message_error', 'Failed to create new user');
-					return Redirect::to('user/add')
-						->with_input();
-				}
-			} else {
-				Session::flash('message_error', 'Login Id is already in use');
+                $success = User::create($userdata);
+                if($success) {
+                    //success login
+                    Session::flash('message', 'Success to create new user');
+                    return Redirect::to('user/index');
+                } else {
+                    Session::flash('message_error', 'Failed to create new user');
+                    return Redirect::to('user/add')
+                        ->with_input();
+                }
+            } else {
+                Session::flash('message_error', 'Login Id is already in use');
                 return Redirect::to('user/add')
                     ->with_input();
-			}	
+            }
         } else {
-			//dd($userdata);
+            //dd($userdata);
             Log::info('Validation fails. error : ' + print_r($validation->errors, true));
             return Redirect::to('user/add')
                 ->with_errors($validation)
-				->with_input();
+                ->with_input();
         }
     }
 
@@ -204,8 +204,8 @@ class User_Controller extends Secure_Controller {
         $rules = array(
             'photo_profile'     => 'mimes:jpg,gif,png|image|max:100',
             'login_id' 	=> 'required|min:5|max:50|alpha_dash',
-			'name'		=> 'required|min:3|max:50',
-			'phone1' 	=> 'required|min:12|max:18',
+            'name'		=> 'required|min:3|max:50',
+            'phone1' 	=> 'required|min:12|max:18',
             'status' 	=> 'required',
         );
         if($method == 'add') {
@@ -237,14 +237,14 @@ class User_Controller extends Secure_Controller {
         if(!$validation->fails()) {
             $user = User::find($all_input['id']);
             $success = User::update($user->id, array(
-                    'password' => $all_input['password']
+                'password' => $all_input['password']
             ));
             if ($success) {
                 Session::flash('message', 'Success update password user '.$user->name);
                 return Redirect::to('user/index');
             } else {
                 Session::flash('message_error', 'Failed update password user '.$user->name);
-            return Redirect::to('user/index');
+                return Redirect::to('user/index');
             }
         } else {
             Log::info('Validation fails. error : ' + print_r($validation->errors, true));
