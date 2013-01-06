e702a6d9bf850a9eaca4ed7be7d8c459a4bea169
Adi <daimagine@gmail.com>
2013-01-06 10:48:19 +0700

    fix item approved to filter only Item or Asset

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => 'localhost',
-			'database' => 'autocare-bna',
+			'database' => 'autocare-bna.test',
 			'username' => 'root',
 			'password' => '',
 			'charset'  => 'utf8',
--- application/models/SubAccountTrx.php
+++ application/models/SubAccountTrx.php
@@ -19,16 +19,14 @@ class SubAccountTrx extends Eloquent {
     public static function getByCriteria($criteria) {
 //        dd($criteria);
         $ate=SubAccountTrx::join('account_transactions', 'sub_account_trx.account_trx_id', '=', 'account_transactions.id');
-        $ate=$ate->join('account', 'account.id', '=', 'account_transactions.account_id');
+        $ate=$ate->join('account', 'account.id', '=', 'sub_account_trx.account_type_id');
         $ate=$ate->join('user', 'user.id', '=', 'account_transactions.create_by');
-//        $ate=$ate->join('account', 'account.id', '=', 'account_transactions.account_id');
         $ate=$ate->where('sub_account_trx.approved_status', '=', $criteria['approved_status']);
         if(isset($criteria['id'])) {$ate=$ate->where('sub_account_trx.id', '=', $criteria['id']);}
         if(isset($criteria['account_trx_id']) && $criteria['account_trx_id']!=null  ) {$ate=$ate->where('sub_account_trx.account_trx_id', '=', $criteria['account_trx_id']);}
         if(isset($criteria['account_trx_type'])) {$ate=$ate->where('account_transactions.type', '=', $criteria['account_trx_type']);}
         if(isset($criteria['account_category'])) {$ate=$ate->where('account.category', '=', $criteria['account_category']);}
         $ate=$ate->where('account_transactions.status', '=', $criteria['account_trx_status']);
-//        $ate=$ate->where('account.category', '=', AccountCategory::ITEM);
 
         return $ate->get('sub_account_trx.*');
     }
